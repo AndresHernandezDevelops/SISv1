67b632edabd41ab59b538e6941fa025587e9bf2d
Andres Hernandez <mymsong@red.eecs.yorku.ca>
2018-03-08 14:09:21 -0500

    added the actual project ot github

---
+++ SIS-1/WebContent/res/mc.js
@@ -0,0 +1,10 @@
+function validate() {
+	
+	var ok = true;
+//	var p = document.getElementById("namePrefix").value;
+//	if (isNaN(p) || p <= 0) {
+//		alert("name prefix invalid!");
+//		ok = false;
+//	}
+	return ok;
+}
---
+++ SIS-1/src/DAO/StudentDAO.java
@@ -0,0 +1,48 @@
+package DAO;
+import bean.*;
+import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class StudentDAO {
+
+	DataSource ds;
+	StudentBean sb;
+	//Statement stmn;
+	
+	public StudentDAO() throws ClassNotFoundException{
+		
+		try{
+			ds = (DataSource) (new InitialContext()).lookup("java:/comp/env/jdbc/EECS");
+			//con = ds.getConnection();
+			//con.createStatement();
+		} catch (NamingException e){
+			e.printStackTrace();
+		}
+	}
+	
+	public Map<String, StudentBean> retrieveStudent(String namePrefix, int credit_taken) throws SQLException{
+		String query = "select * from students where surname like '%" + namePrefix + "%' and credit_taken >= " + credit_taken;
+		Map<String, StudentBean> rv = new HashMap<String, StudentBean>();
+		Connection con = this.ds.getConnection();
+		PreparedStatement p = con.prepareStatement(query);
+		ResultSet r = p.executeQuery();
+		while(r.next())
+		{
+			String name = r.getString("GIVENNAME") + ", " + r.getString("SURNAME");
+			String sid = r.getString("SID");
+			int min_credit = r.getInt("CREDIT_TAKEN");
+			int credit_graduate = r.getInt("CREDIT_GRADUATE");
+			rv.put(sid,new StudentBean(sid, name, min_credit, credit_graduate));
+		}
+		r.close();
+		p.close();
+		con.close();
+		return rv;
+		
+	}
+}
